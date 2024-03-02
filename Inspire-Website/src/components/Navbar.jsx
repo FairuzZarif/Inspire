c1836a073d2da09e423cb6506770dc747f22e725
FairuzZarif <155786897+FairuzZarif@users.noreply.github.com>
2024-03-01 22:22:18 -0700

    TEST

---
+++ Inspire-Website/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <h1 className='font-3xl text-[#b3c7ff]'>INSPIRE</h1> 
+    </div>
+  )
+}
+
+export default Navbar
